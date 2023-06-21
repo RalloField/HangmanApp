a6bd47dc9086920500af8792a82cce78fadb3ce5
Vandevelde <raoul.vandevelde@gmail.com>
2023-06-21 20:57:42 +0200

    Add project files.

---
+++ HangmanApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Random;
+using System.Text;
+
+namespace HangmanApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
